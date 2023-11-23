64a51e99f6d6752948367c00d20286b3f6871e23
島谷　悠 <shimataniyu1209@icloud.com>
2023-11-23 21:17:06 +0900

    カードの並べるのうまくいった

--- front-page.php
+++ front-page.php
@@ -63,10 +63,53 @@ if ($header_image): ?>
     </div>
 </section>
 
+
+<?php
+// カテゴリーのID
+$category_id = 3;
+
+// WP_Queryで指定したカテゴリーの記事を最大5つ取得
+$args = array(
+  'cat' => $category_id,
+  'posts_per_page' => 6, // 最大5つの記事を取得
+);
+$query = new WP_Query($args);
+
+// クエリが記事を持っているか確認
+if ($query->have_posts()) :
+?>
+  <section class="works">
+    <h2 class="c-header works__header">作品</h2>
+    <ul class="col">
+      <?php
+      while ($query->have_posts()) : $query->the_post();
+      ?>
+        <li>
+          <a href="<?php the_permalink() ?>">
+            <div>
+              <?php if (has_post_thumbnail()) : ?>
+                <p class="thumb"><?php echo get_the_post_thumbnail($post->ID, 'full'); ?></p>
+              <?php endif; ?>
+            </div>
+          </a>
+        </li>
+      <?php
+      endwhile;
+      ?>
+    </ul>
+  </section>
+<?php
+  wp_reset_postdata(); // ループ後に元の投稿データを復元
+else :
+  echo '該当する記事はありません。';
+endif;
+?>
+
+
 <?php endif; ?>
 
 <?php query_posts('post_type=post'); ?>
-<?php get_template_part('module_loop'); ?>
+<!-- <?php get_template_part('module_loop'); ?> -->
 
 <?php wp_reset_query(); ?>
 
