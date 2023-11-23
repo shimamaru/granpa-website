e017361b7ba2967fdbec3bf830c6af068349c9ec
島谷　悠 <shimataniyu1209@icloud.com>
2023-11-23 15:40:19 +0900

    切り替わる画像の作成

--- front-page.php
+++ front-page.php
@@ -1,43 +1,64 @@
 <?php get_header();
 $header_image = get_header_image();
 if ($header_image): ?>
-    <div id="mainImg">
+    <!-- <div id="mainImg">
         <img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="<?php bloginfo('description'); ?>">
-    </div>
+    </div> -->
+  <div class="container">
+   <img class="image" src="<?php echo get_template_directory_uri(); ?>/images/syoku-01.jpg" alt="職人1">
+   <img class="image" src="<?php echo get_template_directory_uri(); ?>/images/syoku-02.jpg" alt="職人2">
+   <img class="image" src="<?php echo get_template_directory_uri(); ?>/images/syoku-03.jpg" alt="職人3">
+  </div>
+</div>
 <?php endif; ?>
 
 <?php if (have_posts()) : the_post(); ?>
    <section class="info">
-    <h2 class="info__header">お知らせ・イベント</h2>
-    <?php
-    // カテゴリーのID
-    $category_id = 4;
-
-    // WP_Queryで指定したカテゴリーの記事を最大5つ取得
-    $args = array(
-        'cat' => $category_id,
-        'posts_per_page' => 5, // 最大5つの記事を取得
-    );
-    $query = new WP_Query($args);
-
-    // クエリが記事を持っているか確認
-    if ($query->have_posts()) :
-        echo '<ul>'; // リストの開始
-
-        while ($query->have_posts()) : $query->the_post();
-            echo '<li>';
-            // タイトルにリンクを設定
-            echo '<a href="' . esc_url(get_permalink()) . '">' . get_the_title() . '</a>';
-            echo '</li>';
-        endwhile;
-
-        echo '</ul>'; // リストの終了
-
-        wp_reset_postdata(); // ループ後に元の投稿データを復元
-    else :
-        echo '該当する記事はありません。';
-    endif;
-    ?>
+    <div class="info__inner">
+        <h2 class="info__header">お知らせ・イベント</h2>
+        <?php
+        // カテゴリーのID
+        $category_id = 4;
+    
+        // WP_Queryで指定したカテゴリーの記事を最大5つ取得
+        $args = array(
+            'cat' => $category_id,
+            'posts_per_page' => 5, // 最大5つの記事を取得
+        );
+        $query = new WP_Query($args);
+    
+        // クエリが記事を持っているか確認
+        if ($query->have_posts()) :
+            echo '<ul>'; // リストの開始
+    
+            while ($query->have_posts()) : $query->the_post();
+                echo '<li>';
+                // タイトルにリンクを設定
+                echo '<a href="' . esc_url(get_permalink()) . '">' . get_the_title() . '</a>';
+                echo '</li>';
+            endwhile;
+    
+            echo '</ul>'; // リストの終了
+    
+            wp_reset_postdata(); // ループ後に元の投稿データを復元
+        else :
+            echo '該当する記事はありません。';
+        endif;
+        ?>
+    
+        <div class="info__list">
+         
+            <p class="info__list__text">
+                
+                <a class="info__list__link" href="<?php echo get_category_link($category_id); ?>">
+                
+               <i class="fas fa-list-ul"></i>
+              お知らせ一覧を見る</a>
+              
+            
+   
+        </div>
+    </div>
 </section>
 
 <?php endif; ?>
--- header.php
+++ header.php
@@ -22,6 +22,8 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Shippori+Mincho+B1:wght@600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-******************" crossorigin="anonymous" />
+
   <?php wp_head(); ?>
 </head>
 <body>
